29bd4a22cce6f3ae544093a1d2d0da9c9a9b3b1e
Aaron Allen <amaut31@gmail.com>
2019-04-09 21:52:51 +0000

    Updated title, file name and more.

--- flask_app.py
+++ flask_app.py
@@ -7,3 +7,12 @@ def main():
 
 if __name__ == '__main__':
     app.run()
+
+# @app.route('/puppy_image', methods=['POST'])
+# def puppy_image():
+#     disability = request.form.get('input_disability', ' ')
+#     dropdown = request.form.get('input_dropdown', ' ')
+#     select = request.form.get('input_select', ' ')
+#     freeform = request.form.get('input_freeform', ' ')
+#     return render_template("index.html", input_data=dropdown,
+#                             output="Enter your disability %s." %disability)
